3e1e6710ffa5314f72a21c754803cb32b09b81d9
dxn94 <15531619494@163.com>
2024-05-20 10:36:14 +0800

    Initial commit

---
+++ src/main/java/com/hone/Main.java
@@ -0,0 +1,12 @@
+package com.hone;
+
+/**
+ * @author dxn
+ * @date 2024年05月18日 21:54
+ * @apiNote
+ */
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("Hello world!");
+    }
+}
